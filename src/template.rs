e6c0cd04daa958ec9e2e88eab1131e4dedc64485
Mattias Buelens <mattias@buelens.com>
2021-12-03 23:37:51 +0100

    Add unit tests to template

--- src/template.rs
+++ src/template.rs
@@ -12,3 +12,24 @@ pub fn part1(input: &[i32]) -> i32 {
 pub fn part2(input: &[i32]) -> i32 {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    lazy_static! {
+        static ref TEST_INPUT: &'static str = r"".trim();
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = input_generator(&TEST_INPUT);
+        assert_eq!(part1(&input), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = input_generator(&TEST_INPUT);
+        assert_eq!(part2(&input), 0);
+    }
+}
