e974c0c2939db96a74b237256cbb510f9126cbe8
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-12-07 10:46:48 +0100

    Solve day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -5,17 +5,30 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 
 #[aoc(day7, part1)]
 pub fn part1(input: &[i32]) -> i32 {
-    let min_pos = input.iter().min().unwrap().clone();
-    let max_pos = input.iter().max().unwrap().clone();
+    solve(input, |cur_pos, target_pos| (cur_pos - target_pos).abs())
+}
+
+#[aoc(day7, part2)]
+pub fn part2(input: &[i32]) -> i32 {
+    solve(input, |cur_pos, target_pos| {
+        let steps = (cur_pos - target_pos).abs();
+        // 1 + 2 + 3 + ... + steps = steps * (steps + 1) / 2
+        steps * (steps + 1) / 2
+    })
+}
+
+fn solve(crabs: &[i32], fuel_fn: fn(cur_pos: i32, target_pos: i32) -> i32) -> i32 {
+    let min_pos = crabs.iter().min().unwrap().clone();
+    let max_pos = crabs.iter().max().unwrap().clone();
     let mut best_pos = 0;
     let mut best_fuel = i32::MAX;
     'outer: for pos in min_pos..=max_pos {
         let mut fuel = 0;
-        for &crab in input {
+        for &crab in crabs {
             if fuel >= best_fuel {
                 continue 'outer;
             }
-            fuel += (crab - pos).abs();
+            fuel += fuel_fn(crab, pos);
         }
         if fuel < best_fuel {
             best_pos = pos;
@@ -23,11 +36,6 @@ pub fn part1(input: &[i32]) -> i32 {
         }
     }
     best_fuel
-}
-
-#[aoc(day7, part2)]
-pub fn part2(input: &[i32]) -> i32 {
-    todo!()
 }
 
 #[cfg(test)]
@@ -45,6 +53,6 @@ mod tests {
     #[test]
     fn test_part2() {
         let input = input_generator(&TEST_INPUT);
-        assert_eq!(part2(&input), 0);
+        assert_eq!(part2(&input), 168);
     }
 }
