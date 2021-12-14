e7d90fd96955fec97cfcbdc42be513c601e45b9a
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-12-14 12:58:55 +0100

    Refactor

--- src/day14.rs
+++ src/day14.rs
@@ -25,19 +25,23 @@ fn step(polymer: &[char], rules: &RuleMap) -> Vec<char> {
     }).chain(polymer.last().cloned()).collect()
 }
 
-#[aoc(day14, part1)]
-pub fn part1((template, rules): &Input) -> usize {
+fn solve_part1((template, rules): &Input, steps: usize) -> u64 {
     let mut polymer = template.clone();
-    for _ in 1..=10 {
+    for _ in 1..=steps {
         polymer = step(&polymer, rules);
     }
-    let mut counts = HashMap::<char, usize>::new();
+    let mut counts = HashMap::<char, u64>::new();
     for &c in polymer.iter() {
         *counts.entry(c).or_default() += 1;
     }
     let min_count = *counts.values().min().unwrap();
     let max_count = *counts.values().max().unwrap();
     max_count - min_count
+}
+
+#[aoc(day14, part1)]
+pub fn part1(input: &Input) -> u64 {
+    solve_part1(input, 10)
 }
 
 type PairCounts = HashMap<(char, char), u64>;
@@ -75,16 +79,20 @@ fn count_elements(pair_counts: &PairCounts, last_element: char) -> ElementCounts
     counts
 }
 
-#[aoc(day14, part2)]
-pub fn part2((template, rules): &Input) -> u64 {
+fn solve_part2((template, rules): &Input, steps: usize) -> u64 {
     let mut pair_counts = make_pair_counts(template);
-    for _ in 1..=40 {
+    for _ in 1..=steps {
         pair_counts = step_part2(&pair_counts, rules);
     }
     let counts = count_elements(&pair_counts, *template.last().unwrap());
     let min_count = *counts.values().min().unwrap();
     let max_count = *counts.values().max().unwrap();
     max_count - min_count
+}
+
+#[aoc(day14, part2)]
+pub fn part2(input: &Input) -> u64 {
+    solve_part2(input, 40)
 }
 
 #[cfg(test)]
@@ -123,5 +131,11 @@ CN -> C".trim();
     fn test_part2() {
         let input = input_generator(&TEST_INPUT);
         assert_eq!(part2(&input), 2188189693529);
+    }
+
+    #[test]
+    fn test_part1_using_part2() {
+        let input = input_generator(&TEST_INPUT);
+        assert_eq!(solve_part2(&input, 10), 1588);
     }
 }
