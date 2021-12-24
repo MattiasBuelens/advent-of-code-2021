ec7add38b6359519254037c34e101c34db4d7eee
Mattias Buelens <mattias@buelens.com>
2021-12-24 20:28:45 +0100

    Implement ALU

--- src/day24.rs
+++ src/day24.rs
@@ -69,8 +69,76 @@ impl FromStr for Variable {
     }
 }
 
+struct Alu {
+    program: Vec<Instruction>,
+    variables: [i32; 4],
+}
+
+impl Alu {
+    fn new(program: Vec<Instruction>) -> Self {
+        Self {
+            program,
+            variables: [0; 4],
+        }
+    }
+
+    fn run(&mut self, input: &[i32]) {
+        let mut input = input.into_iter().copied();
+        for instruction in self.program.clone() {
+            self.step(instruction, &mut input);
+        }
+    }
+
+    fn step(&mut self, instruction: Instruction, input: &mut impl Iterator<Item = i32>) {
+        match instruction {
+            Instruction::Input(a) => {
+                *self.var_mut(a) = input.next().expect("missing input");
+            }
+            Instruction::Add(a, b) => {
+                *self.var_mut(a) += self.get(b);
+            }
+            Instruction::Mul(a, b) => {
+                *self.var_mut(a) *= self.get(b);
+            }
+            Instruction::Div(a, b) => {
+                *self.var_mut(a) = *self.var(a) / self.get(b);
+            }
+            Instruction::Mod(a, b) => {
+                *self.var_mut(a) = *self.var(a) % self.get(b);
+            }
+            Instruction::Eql(a, b) => {
+                *self.var_mut(a) = if *self.var(a) == self.get(b) { 1 } else { 0 };
+            }
+        }
+    }
+
+    fn get(&self, operand: Operand) -> i32 {
+        match operand {
+            Operand::Var(var) => *self.var(var),
+            Operand::Num(val) => val,
+        }
+    }
+
+    fn var(&self, variable: Variable) -> &i32 {
+        &self.variables[Self::var_index(variable)]
+    }
+
+    fn var_mut(&mut self, variable: Variable) -> &mut i32 {
+        &mut self.variables[Self::var_index(variable)]
+    }
+
+    fn var_index(variable: Variable) -> usize {
+        match variable {
+            Variable::W => 0,
+            Variable::X => 1,
+            Variable::Y => 2,
+            Variable::Z => 3,
+        }
+    }
+}
+
 #[aoc_generator(day24)]
-pub fn input_generator(input: &str) -> Vec<Instruction> {
+pub fn parse_program(input: &str) -> Vec<Instruction> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
@@ -93,14 +161,69 @@ mod tests {
     }
 
     #[test]
+    fn test_negate() {
+        let program = parse_program(
+            r"
+inp x
+mul x -1"
+                .trim(),
+        );
+        let mut alu = Alu::new(program);
+        alu.run(&[123]);
+        assert_eq!(*alu.var(Variable::X), -123);
+    }
+
+    #[test]
+    fn test_three_times() {
+        let program = parse_program(
+            r"
+inp z
+inp x
+mul z 3
+eql z x"
+                .trim(),
+        );
+        let mut alu = Alu::new(program);
+        alu.run(&[2, 6]);
+        assert_eq!(*alu.var(Variable::Z), 1);
+        alu.run(&[2, 7]);
+        assert_eq!(*alu.var(Variable::Z), 0);
+    }
+
+    #[test]
+    fn test_binary() {
+        let program = parse_program(
+            r"
+inp w
+add z w
+mod z 2
+div w 2
+add y w
+mod y 2
+div w 2
+add x w
+mod x 2
+div w 2
+mod w 2"
+                .trim(),
+        );
+        let mut alu = Alu::new(program);
+        alu.run(&[0b1010]);
+        assert_eq!(*alu.var(Variable::W), 1);
+        assert_eq!(*alu.var(Variable::X), 0);
+        assert_eq!(*alu.var(Variable::Y), 1);
+        assert_eq!(*alu.var(Variable::Z), 0);
+    }
+
+    #[test]
     fn test_part1() {
-        let input = input_generator(&TEST_INPUT);
+        let input = parse_program(&TEST_INPUT);
         assert_eq!(part1(&input), 0);
     }
 
     #[test]
     fn test_part2() {
-        let input = input_generator(&TEST_INPUT);
+        let input = parse_program(&TEST_INPUT);
         assert_eq!(part2(&input), 0);
     }
 }
